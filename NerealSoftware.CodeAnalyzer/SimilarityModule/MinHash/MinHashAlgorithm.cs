2d788b1de6055e4448d8d647627c8a13c69af1e3
Aleksey Senilov <thenn.erannor@gmail.com>
2019-09-28 10:09:29 +0300

    First MinHash implementation

---
+++ NerealSoftware.CodeAnalyzer/SimilarityModule/MinHash/MinHashAlgorithm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.HashFunction;
+using System.Data.HashFunction.MurmurHash;
+
+namespace SimilarityModule.MinHash
+{
+    public class MinHashAlgorithm
+    {
+        private readonly int _hashCount;
+        private readonly IHashFunction[] _hashFunctions;
+
+        public MinHashAlgorithm(int hashCount)
+        {
+            _hashCount=hashCount;
+            var factory = MurmurHash3Factory.Instance;
+            _hashFunctions = new IHashFunction[_hashCount];
+            for (int i = 0; i < _hashCount; i++)
+            {
+                var config = new MurmurHash3Config
+                {
+                    HashSizeInBits = 32,
+                    Seed = uint.MaxValue / (uint) hashCount * (uint) i
+                };
+                _hashFunctions[i] = factory.Create(config);
+            }
+        }
+
+        public uint[] Calculate(int[] data, int blockSize)
+        {
+            var result = new uint[_hashCount];
+            var bytes = new byte[blockSize * sizeof(int)];
+            for (int i = 0; i < _hashCount; i++)
+            {
+                var func = _hashFunctions[i];
+                var minValue = uint.MaxValue;
+                for (int j = 0; j < data.Length-blockSize+1; j++)
+                {
+                    Buffer.BlockCopy(data, j * sizeof(int), bytes, 0, bytes.Length);
+                    var hash = func.ComputeHash(bytes);
+                    var value = BitConverter.ToUInt32(hash.Hash, 0);
+                    if (value < minValue)
+                    {
+                        minValue = value;
+                    }
+                }
+
+                result[i] = minValue;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NerealSoftware.CodeAnalyzer/UnitTests/MinHashAlgorithmTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimilarityModule.MinHash;
+
+namespace SimilarityModule.Tests
+{
+    [TestClass]
+    public class MinHashAlgorithmTests
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            var algorithm = new MinHashAlgorithm(100);
+            var data = new int[1000];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = i;
+            }
+
+            var result1 = algorithm.Calculate(data, 4);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = i + 100;
+            }
+            var result2 = algorithm.Calculate(data, 4);
+
+            for (int i = 0; i < result1.Length; i++)
+            {
+                if (result1[i] != result2[i])
+                {
+                    Console.WriteLine($"Result diffs at index {i}");
+                }
+            }
+        }
+    }
+}
