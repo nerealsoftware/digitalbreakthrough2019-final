786858f1aa3361c9c0a1f2d9c283b2e0808ead7c
orrollo <orrollo@gmail.com>
2019-09-28 12:34:02 +0300

    первый проход чистки исходника через roslyn

---
+++ NerealSoftware.CodeAnalyzer/UnitTests/TestHelper.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using System.Reflection;
+
+namespace UnitTests
+{
+    public static class TestHelper
+    {
+        public static string ReadResource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
